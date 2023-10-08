3af005fffa3e0b6c8411c3266a3a5f2eab986d43
Hinaichigo25 <44768592+Hinaichigo25@users.noreply.github.com>
2023-10-08 14:00:01 -0600

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace RecRoomPainter2;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void LoadButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RecRoomPainter2
+ A cross-platform painting tool for drawing images on a canvas in Rec Room.
